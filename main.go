9ce81505dc8aa1f168fa084c073c019e8c6f2986
Stefan Luecke <glaxx@glaxx.net>
2017-02-28 18:36:14 +0100

    Added boilerplate code

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	log "github.com/Sirupsen/logrus"
+	//"github.com/jung-kurt/gofpdf"
+	"github.com/muesli/smolder"
+	"gopkg.in/gcfg.v1"
+	"net/http"
+)
+
+type Config struct {
+	Network struct {
+		ListenTo string
+	}
+	Templates struct {
+		Directory string
+	}
+	Log struct {
+		File  string
+		Level string
+	}
+}
 
 func main() {
-	fmt.Println(".")
+	cfg := Config{}
+	err := gcfg.ReadFileInto(&cfg, "/etc/quit/quit.conf")
+	if err != nil {
+		log.WithField("Error", err).Fatalln("Could not read config file")
+	}
+
+	lvl, err := log.ParseLevel(cfg.Log.Level)
+	if err != nil {
+		log.WithField("Error", err).Error("Could not parse config level, defaulting to info")
+		log.SetLevel(log.InfoLevel)
+	} else {
+		log.SetLevel(lvl)
+	}
+
+	log.Info("Config read successfull")
+
+	wsContainer := smolder.NewSmolderContainer(
+		smolder.APIConfig{
+			BaseURL:    cfg.Network.ListenTo,
+			PathPrefix: "",
+		}, nil, nil)
+
+	server := http.Server{Addr: cfg.Network.ListenTo, Handler: wsContainer}
+	log.Fatal(server.ListenAndServe())
 }
